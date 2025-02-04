b712149715a685b0f5ac293a12a57cc33a13b286
Mano Ranjan N <manoranjann@Manos-MacBook-Pro.local>
2025-02-04 16:05:31 +0530

    added different schema in proto file nested, list and dictionary/maps

--- src/main/scala/experiment.scala
+++ src/main/scala/experiment.scala
@@ -1,24 +1,27 @@
 package example
 
 
-import protobuf.employeeSchema.Employee
-import protobuf.employeeSchema.Phone
+import protobuf.employeeSchema.NestedEmployeeType
+import protobuf.employeeSchema.NestedEmployeeType.Address
+import protobuf.employeeSchema.SkillsList
+import protobuf.employeeSchema.EmployeeMapDict
+
 
 
 object protobufOps {
-  def serializer(emp: Employee): Array[Byte] = {
+  def serializer(emp: NestedEmployeeType): Array[Byte] = {
     emp.toByteArray
   }
 
 
-  def deserializer(bytes: Array[Byte]): Employee = {
-    Employee.parseFrom(bytes)
+  def deserializer(bytes: Array[Byte]): NestedEmployeeType = {
+    NestedEmployeeType.parseFrom(bytes)
   }
 }
 
 object Experiment {
   def main(args: Array[String]) = {
-    val emp = Employee(name="manosec", age=12, number="1234567890")
+    val emp = NestedEmployeeType(name="manosec", age=12, address=Some(Address(city="bangalore", state="karnataka")), skills=Some(SkillsList(List("java", "scala"))), additional=Some(EmployeeMapDict(Map("xyz" -> "ml engineer", "abc" -> "software engineer"))))
 
     val bytes = protobufOps.serializer(emp)
 
